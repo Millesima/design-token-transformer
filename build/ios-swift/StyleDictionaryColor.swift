d5d2874c2592e82309e5e6e881be5ea0cdba8177
aperrinStc <aperrinStc@users.noreply.github.com>
2023-10-10 12:43:10 +0000

    Test

--- build/ios-swift/StyleDictionaryColor.swift
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -4,18 +4,114 @@
 //
 
 // Do not edit directly
-// Generated on Mon, 08 May 2023 07:37:37 GMT
+// Generated on Tue, 10 Oct 2023 12:43:10 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryColor {
-    public static let colorsEmpty = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0)
-    public static let colorsMultipleFills0 = UIColor(red: 0.251, green: 1.000, blue: 0.729, alpha: 1)
-    public static let colorsMultipleFills1 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.10196078431372549)
-    public static let colorsRefBlue = UIColor(red: 0.016, green: 0.290, blue: 1.000, alpha: 1)
-    public static let colorsSemiRed = UIColor(red: 1.000, green: 0.373, blue: 0.016, alpha: 0.5019607843137255)
-    public static let colorsSingleBlue = UIColor(red: 0.016, green: 0.290, blue: 1.000, alpha: 1)
-    public static let colorsSpecialCharacters = UIColor(red: 0.251, green: 0.875, blue: 0.314, alpha: 1)
-    public static let colorsSpecialCharactersNderung = UIColor(red: 0.204, green: 0.337, blue: 0.686, alpha: 1)
+    public static let backgroundPrimaryActive = #9f9f9fff
+    public static let backgroundPrimaryActive = "#ffffff00"
+    public static let backgroundPrimaryDisable = #efefefff
+    public static let backgroundPrimaryDisable = "#ffffff00"
+    public static let backgroundPrimaryDisable = #f5f5f5ff
+    public static let backgroundPrimaryEnable = #171717ff
+    public static let backgroundPrimaryEnable = "#ffffff00"
+    public static let backgroundPrimaryEnable = #ffffffff
+    public static let backgroundPrimaryErrorEnable = #fee8e7ff
+    public static let backgroundPrimaryHover = #636363ff
+    public static let backgroundPrimaryHover = "#ffffff00"
+    public static let backgroundPrimaryHover = #ffffffff
+    public static let backgroundPrimarySelect = #ffffffff
+    public static let backgroundSecondaryActive = #f6f5f3ff
+    public static let backgroundSecondaryDisable = #efefefff
+    public static let backgroundSecondaryEnable = #ffffff00
+    public static let backgroundSecondaryHover = #f6f5f3ff
+    public static let backgroundTertiaryActive = #f6f5f3ff
+    public static let backgroundTertiaryDisable = #efefefff
+    public static let backgroundTertiaryEnable = #ffffff00
+    public static let backgroundTertiaryHover = #f6f5f3ff
+    public static let bronze = #d89f52ff
+    public static let dark100 = #000000ff
+    public static let dividerPrimaryEnable = #efefefff
+    public static let gold = #eec66cff
+    public static let green100 = #64c09dff
+    public static let green200 = #4eab85ff
+    public static let green300 = #3e9570ff
+    public static let green400 = #31825fff
+    public static let green50 = #dffee7ff
+    public static let green500 = #256f4eff
+    public static let green600 = #195c3dff
+    public static let green700 = #10462dff
+    public static let grey10 = #f6f5f3ff
+    public static let grey100 = #171717ff
+    public static let grey20 = #f5f5f5ff
+    public static let grey30 = #efefefff
+    public static let grey40 = #dbdbdbff
+    public static let grey50 = #bdbdbdff
+    public static let grey60 = #9f9f9fff
+    public static let grey70 = #818181ff
+    public static let grey80 = #636363ff
+    public static let grey90 = #454545ff
+    public static let neutral0 = #ffffffff
+    public static let opacity0 = #ffffff00
+    public static let red10 = #fee8e7ff
+    public static let red20 = #fb9e97ff
+    public static let red30 = #ff7e6fff
+    public static let red40 = #f45050ff
+    public static let red50 = #d44b41ff
+    public static let red60 = #b23c33ff
+    public static let red70 = #8c2f24ff
+    public static let red80 = #5a0a00ff
+    public static let silver = #cfdae8ff
+    public static let strokePriamryHover = #bdbdbdff
+    public static let strokePrimaryActive = #ffffff00
+    public static let strokePrimaryActive = "#dbdbdbff"
+    public static let strokePrimaryDisable = #ffffffff
+    public static let strokePrimaryDisable = "#bdbdbdff"
+    public static let strokePrimaryDisable = #ffffff00
+    public static let strokePrimaryEnable = #ffffff00
+    public static let strokePrimaryEnable = "#bdbdbdff"
+    public static let strokePrimaryErrorEnable = #f45050ff
+    public static let strokePrimaryHover = #ffffff00
+    public static let strokePrimaryHover = "#dbdbdbff"
+    public static let strokePrimarySelect = #171717ff
+    public static let strokeSecondaryActive = #171717ff
+    public static let strokeSecondaryDisable = #ffffff00
+    public static let strokeSecondaryEnable = #171717ff
+    public static let strokeSecondaryHover = #171717ff
+    public static let strokeTertiaryActive = #ffffffff
+    public static let strokeTertiaryDisable = #ffffff00
+    public static let strokeTertiaryEnable = #ffffffff
+    public static let strokeTertiaryHover = #ffffffff
+    public static let strokeTextDisable = #9f9f9fff
+    public static let strokeTextEnable = #171717ff
+    public static let strokeTextHover = #171717ff
+    public static let strokeTextSelect = #171717ff
+    public static let surfacePrimary = #ffffffff
+    public static let surfaceSecondary = #f6f5f3ff
+    public static let textGhostActive = #9f9f9fff
+    public static let textGhostDisable = #bdbdbdff
+    public static let textGhostEnable = #171717ff
+    public static let textGhostHover = #9f9f9fff
+    public static let textInversePrimaryEnable = #ffffffff
+    public static let textPrimaryActive = #ffffffff
+    public static let textPrimaryActive = "#171717ff"
+    public static let textPrimaryDisable = #bdbdbdff
+    public static let textPrimaryDisable = "#bdbdbdff"
+    public static let textPrimaryEnable = #ffffffff
+    public static let textPrimaryEnable = "#171717ff"
+    public static let textPrimaryEnable = #171717ff
+    public static let textPrimaryHover = #ffffffff
+    public static let textPrimaryHover = "#171717ff"
+    public static let textPrimaryPlaceholder = #9f9f9fff
+    public static let textSecondaryActive = #9f9f9fff
+    public static let textSecondaryDisable = #bdbdbdff
+    public static let textSecondaryEnable = #171717ff
+    public static let textSecondaryEnable = #9f9f9fff
+    public static let textSecondaryHover = #171717ff
+    public static let textTertiaryActive = #9f9f9fff
+    public static let textTertiaryDisable = #bdbdbdff
+    public static let textTertiaryEnable = #171717ff
+    public static let textTertiaryHover = #171717ff
 }
