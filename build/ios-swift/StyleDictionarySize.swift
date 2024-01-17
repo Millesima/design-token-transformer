b6a84bfec0448c1e1795e7ae03a680cae6e637fb
aperrinStc <aperrinStc@users.noreply.github.com>
2024-01-17 13:25:38 +0000

    test - Exclude props

---
+++ build/ios-swift/StyleDictionarySize.swift
@@ -0,0 +1,28 @@
+
+//
+// StyleDictionarySize.swift
+//
+
+// Do not edit directly
+// Generated on Wed, 17 Jan 2024 13:25:38 GMT
+
+
+import UIKit
+
+public enum StyleDictionarySize {
+    public static let displayFontWeight = 600
+    public static let headingLargeFontWeight = 600
+    public static let headingMediumFontWeight = 600
+    public static let headingSmallFontWeight = 600
+    public static let headingXlargeFontWeight = 600
+    public static let labelLargeFontWeight = 500
+    public static let labelMediumFontWeight = 500
+    public static let labelSmallFontWeight = 500
+    public static let labelXlargeFontWeight = 500
+    public static let paragraphLargeFontWeight = 400
+    public static let paragraphMediumFontWeight = 400
+    public static let paragraphMediumUnderlineFontWeight = 400
+    public static let paragraphSmallFontWeight = 400
+    public static let paragraphXlargeFontWeight = 400
+    public static let paragraphXsmallFontWeight = 400
+}
